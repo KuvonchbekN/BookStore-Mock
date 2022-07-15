3437d98b81c502d85b03d2db328381556cf7f71f
KuvonchbekN <knormakhmatov@students.wiut.uz>
2022-07-15 16:00:25 +0500

    added constructor to book

--- src/main/java/com/aman/bookstore/dataLayer/entity/Book.java
+++ src/main/java/com/aman/bookstore/dataLayer/entity/Book.java
@@ -5,4 +5,11 @@ public class Book {
     public String title;
     public String bookAuthorName;
     public BookGenre bookGenre;
+
+    public Book(ID id, String title, String bookAuthorName, BookGenre bookGenre) {
+        this.id = id;
+        this.title = title;
+        this.bookAuthorName = bookAuthorName;
+        this.bookGenre = bookGenre;
+    }
 }
